6f930efa631d9e341c2aff0bffad902d12961202
aksh044 <126679868+aksh044@users.noreply.github.com>
2023-09-30 16:27:28 +0530

    Add files via upload

---
+++ pet.js
@@ -0,0 +1,31 @@
+// let nav1=document.getElementsByTagName("nav")
+// nav1[0].style.color="red"
+
+let tle=document.getElementById("karan")
+
+// tle.hasAttribute("hidden")
+
+// Array.from(tle).forEach((ele)=>{
+//     ele.style.background="green";
+// })
+// console.log(tle[0])
+// ul1.firstel?
+// tle[0].firstElementChild.style.color="red"
+// tle[0].lastElementChild.style.color="red"
+
+// let abc=document.getElementsByi("hero-content")
+
+tle.classList.add("cta-button")
+// let divw=document.getElementsByClassName("centre")
+// // divw.innerHTML="<h2>TMKOC</h2>"
+
+// abc.insertAdjacentHTML("afterbegin","<h2>TMKOC</h2>")
+
+
+// abc.before(div);
+// let pqr=document.querySelectorAll(".product-item")
+// console.log(pqr);
+
+// abc.style.color="red";
+
+// pqr[0].style.color="pink"
